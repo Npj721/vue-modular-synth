253efdc1dfd3bc9a0a35f267e92bae3eeec00ec0
Nicolas Petit-Jean <nicolas.petit-jean@hotmail.com>
2026-01-24 20:52:02 +0100

    utilisation de patch depuis syntheditor dans le keyboard, limité pour le moment sur le voicePatch

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
   import { reactive } from "vue"
   import SynthEditor from './components/SynthEditor.vue'
+  import SynthKeyboard from './components/SynthKeyboard.vue'
   const patch = reactive({})
 
 </script>
 
 <template>
   <SynthEditor v-model:patch="patch"/>
+  <SynthKeyboard :patch="patch.voicePatch" />
  <h1>patch</h1>
     <div>
         {{ patch }}
