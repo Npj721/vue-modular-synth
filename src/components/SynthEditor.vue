fc47e16788f4fb3530dd8e2eb34c9c7a89155b41
Nicolas Petit-Jean <nicolas.petit-jean@hotmail.com>
2026-01-24 23:21:14 +0100

    style

--- src/components/SynthEditor.vue
+++ src/components/SynthEditor.vue
@@ -59,12 +59,40 @@ const emit = defineEmits(["update:patch"])
 </script>
 <template>
 <div>
-    <h1>{{ currentPatch }}</h1>
+    <h1>Sélection du patch</h1>
     <div>
-        <button @click="toggleVoicePatch">Voice</button>
-        <button @click="toggleMainPatch">Main</button>
+        <button :class="currentPatch === 'voice' ? 'button actif' : 'button'" @click="toggleVoicePatch">Voice</button>
+        <button :class="currentPatch === 'main' ? 'button actif' : 'button'" class="button" @click="toggleMainPatch">Main</button>
     </div>
     <PatchEditor v-model:patch="voicePatch" v-show="currentPatch === 'voice'"></PatchEditor>
     <PatchEditor v-model:patch="mainPatch" v-show="currentPatch === 'main'"></PatchEditor>
 </div>
 </template>
+
+<style scoped>
+h1{
+  color: rgb(0, 39, 33);;
+
+}
+.button {
+  margin-right: 12px;
+  padding: 6px 12px;
+  border: none;
+  background: rgb(1, 122, 102);
+  color: white;
+  font-weight: bold;
+  font-size: large;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: transform 0.1s;
+}
+
+.button.actif {
+  background: #00c9a7;
+  color: white;
+}
+
+.button:hover {
+  transform: scale(1.05);
+}
+</style>
